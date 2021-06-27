2c9bf53c3e0e3cce9f9acd5d50c245df9053dd60
Shivaji Chalise <respond2shivaji@gmail.com>
2021-06-27 15:00:12 +0545

    Added database conf and created user, product and order model

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -20,9 +24,10 @@ app.get('/api/product/:id', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
-console.log(process)
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 )
