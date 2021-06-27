fd2d31566910bfe8ac8c9473e41b3c332e8ec2fc
Shivaji Chalise <respond2shivaji@gmail.com>
2021-06-27 09:22:49 +0545

    Serving products; simple backend routes

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running..')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = product.find((p) => p._id === req.params.id)
+  res.send(product)
+})
+
+app.listen(5000, console.log('Server running on 5000'))
