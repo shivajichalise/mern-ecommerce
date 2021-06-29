b11794fc49dd544a4a73cd91db0d62e89e8c4a75
Shivaji Chalise <respond2shivaji@gmail.com>
2021-06-29 19:00:15 +0545

    Added Quantity Select and Add to cart button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,22 +5,22 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <LinkContainer to='/'>
             <Navbar.Brand>Eshop</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
+                  <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
+              <LinkContainer to='/login'>
                 <Nav.Link>
-                  <i className="fas fa-user"></i> Sign In
+                  <i className='fas fa-user'></i> Sign In
                 </Nav.Link>
               </LinkContainer>
             </Nav>
